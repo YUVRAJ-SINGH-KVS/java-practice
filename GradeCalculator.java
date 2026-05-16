b83a6157050c46282227cfeafeabc99431c7993e
YUVRAJ-SINGH-KVS <yuvraj20122004@gmail.com>
2026-05-16 22:13:04 +0530

    created a grade evaluation calculator

---
+++ GradeCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        System.out.println("enter marks:-");
+        Scanner s = new Scanner(System.in);
+
+        double marks = s.nextDouble();
+        if (marks>=60){
+            System.out.println("Grade :- A");
+        }else if (marks>=40 && marks<60){
+            System.out.println("Grade :- B");
+        }else if (marks>=20 && marks<40){
+            System.out.println("Grade :- C");
+        }else{
+            System.out.println("Grade :- F");
+        }
+    }
+}
